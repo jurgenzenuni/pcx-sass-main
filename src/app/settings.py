8be79496f3b096043c1e2f576f32d28f81395fe4
jurg <jurgen.zenuni@outlook.com>
2025-02-13 23:26:55 -0500

    updated base, docker, settings for favicon issue

--- src/app/settings.py
+++ src/app/settings.py
@@ -158,7 +158,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # Or wherever you want collected files to go
 
 # After STATIC_ROOT setting
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
