699eeed02f87342a50a304e653be67397ff8bc5f
jurg <jurgen.zenuni@outlook.com>
2025-02-15 01:11:13 -0500

    error buidling

--- src/app/settings.py
+++ src/app/settings.py
@@ -151,13 +151,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Make sure this exists
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'app/static'),
 ]
 
-# After STATIC_ROOT setting
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
